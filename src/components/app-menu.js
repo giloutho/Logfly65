77773cdb6ca82ac154a419d460874da65f01f701
Mac16 <gilouthomas@gmail.com>
2025-08-23 17:05:30 +0200

    Full screen toggle added

--- src/components/app-menu.js
+++ src/components/app-menu.js
@@ -10,9 +10,21 @@ export class AppMenu extends HTMLElement {
             <li class="nav-item"><a class="nav-link" href="#settings">Paramètres</a></li>
             <li class="nav-item"><a class="nav-link" href="#about">À propos</a></li>
           </ul>
+          <button id="fullscreen-toggle" class="btn btn-outline-light ms-auto" title="Plein écran">
+            <i class="bi bi-arrows-fullscreen"></i>
+          </button>
         </div>
       </nav>
     `;
+
+    // Ajout du comportement toggle plein écran
+    this.querySelector('#fullscreen-toggle').addEventListener('click', () => {
+      if (!document.fullscreenElement) {
+        document.documentElement.requestFullscreen();
+      } else {
+        document.exitFullscreen();
+      }
+    });
   }
 }
 
