bce211624387e16865e422b20219d10ddaa3987c
Mac16 <gilouthomas@gmail.com>
2025-10-09 23:45:05 +0200

    Console.log removed

--- src/ipcmain/ports-list.js
+++ src/ipcmain/ports-list.js
@@ -10,7 +10,7 @@ ipcMain.handle('gps:serial', async (event, args) => {
     console.log('[check-serial-gps] called ')
     const result = await listPorts()
     if (result instanceof Array) { 
-        console.log('[check-serial-gps] found ports: ', result);
+        //console.log('[check-serial-gps] found ports: ', result);
         return { success: true, portsarray: result };
     } else {
         console.error('[check-serial-gps] error: ', result);
