4bfbce7c5bf501ec018fcf804a5df95397ad3893
Mac16 <gilouthomas@gmail.com>
2025-10-10 23:31:44 +0200

    initial commit

---
+++ src/ipcmain/gpsdump-flight.js
@@ -0,0 +1,108 @@
+const {app} = require('electron')
+const path = require('path')
+const fs = require('fs')
+const log = require('electron-log')
+const gpsDumpFiles = require('../js/gpsdump-settings.js')
+const gpsDumpParams = gpsDumpFiles.getParam()
+const Store = require('electron-store').default;
+const store = new Store()
+const specOS = store.get('specOS')
+
+function getGpsdumpFlight(gpsParam, flightIndex) {
+    // gpsParam contains parameters for GpsDump
+    // something like -giq,-cu.usbserial-14140,FlymasterSD
+    // First one is gps type, second serial port
+    console.log('flightIndex : '+flightIndex+' gpsParam : '+gpsParam)
+    let data
+    const execFileSync = require('child_process').execFileSync
+    if (process.env.NODE_ENV !== 'production') {
+        gpsDumpPath = path.join(path.dirname(__dirname), '../bin_ext',gpsDumpParams[specOS].gpsdump);
+    } else {
+        gpsDumpPath = path.join(app.getAppPath(), '../bin_ext',gpsDumpParams[specOS].gpsdump);
+    }
+    const tempFileName  = path.join(app.getPath('temp'), 'gpsdump.igc')
+    if (fs.existsSync(tempFileName)) {
+      try {
+        fs.unlinkSync(tempFileName)
+      } catch(err) {
+        log.error('[getGpsdumpFlight] The gpsDump temporary file was not deleted : '+err)
+      }
+    }
+    const paramFile = gpsDumpParams[specOS].temp+tempFileName
+    let paramFlightIndex
+    let callString
+    let res = null 
+    try {
+      const gpsParamArray = gpsParam.split(",")
+      const paramGPS = gpsParamArray[0]
+      const paramPort = gpsParamArray[1]
+      const gpsModel = gpsParamArray[2]
+      
+      const wNoWin = '/win=0'  
+      const wExit = '/exit'  
+      switch (gpsModel) {
+        case 'flysd':
+          // e.g. -f4
+          flightIndex +=1          
+          paramFlightIndex = gpsDumpParams[specOS].track+flightIndex.toString()
+          break
+        case 'flyold':
+          if (specOS != 'win') {
+            flightIndex +=1
+          }
+          paramFlightIndex = gpsDumpParams[specOS].track+flightIndex.toString()         
+          break                
+        case 'fly20':
+          if (specOS != 'win') {
+            flightIndex +=1
+          }
+          paramFlightIndex = gpsDumpParams[specOS].track+flightIndex.toString()                              
+          break      
+        case 'fly15':
+          if (specOS != 'win') {
+            flightIndex +=1
+          }
+          console.log('flightIndex in Flytec15 : '+flightIndex)
+          paramFlightIndex = gpsDumpParams[specOS].track+flightIndex.toString()               
+          break    
+      }
+      switch (specOS) {
+        case 'win':            
+            callString = path.basename(gpsDumpPath)+' '+wNoWin+' '+paramPort+' '+paramGPS+' '+paramFile+' '+paramFlightIndex+' '+wExit
+            console.log(callString)
+            data = execFileSync(gpsDumpPath, [wNoWin, paramPort, paramGPS, paramFile, paramFlightIndex, wExit ])
+            // L5 -> new String[]{pathGpsDump,wNoWin,wComPort,sTypeGps, logIGC, numberIGC, wExit};
+            break
+        case 'mac32':
+          callString = path.basename(gpsDumpPath)+' '+paramGPS+' '+paramFile+' '+paramFlightIndex
+          console.log(callString)
+          data = execFileSync(gpsDumpPath, [paramGPS,paramFile, paramFlightIndex])
+          // L5 -> new String[]{pathGpsDump,sTypeGps, name32IGC, numberIGC}; 
+          break            
+        case 'mac64':
+            callString = path.basename(gpsDumpPath)+' '+paramGPS+' '+paramPort+' '+paramFile+' '+paramFlightIndex
+            console.log(callString)
+            data = execFileSync(gpsDumpPath, [paramGPS,paramPort,paramFile,paramFlightIndex])
+            // L5 -> new String[]{pathGpsDump,sTypeGps, macPort, name64IGC, numberIGC};
+            break
+        case 'linux':
+            callString = path.basename(gpsDumpPath)+' '+paramGPS+' '+paramPort+' '+paramFile+' '+paramFlightIndex
+            console.log(callString)
+            data = execFileSync(gpsDumpPath, [paramGPS,paramPort,paramFile,paramFlightIndex])
+            // L5 -> new String[]{pathGpsDump,sTypeGps, linuxPort, tempIGC, numberIGC};
+            break
+      }
+ 
+      // data has been declared but not not necessarily initialized if the communication fails
+      if (data) {
+          res = tempFileName
+      }     
+    } catch (error) {
+      res = 'Error on calling ['+callString+']'
+      log.error(res+' : '+error)
+    }
+  
+    return res    
+  }
+
+  module.exports.getGpsdumpFlight = getGpsdumpFlight
