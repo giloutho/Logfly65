9140fe58cc42caa35b83d8fa266f8cdac9ee244e
Mac16 <gilouthomas@gmail.com>
2025-10-24 22:05:25 +0200

    right panel added

--- src/components/pages/home-page.js
+++ src/components/pages/home-page.js
@@ -9,17 +9,19 @@ export class HomePage extends HTMLElement {
                     <div class="col-lg-7 h-100">
                          <log-map class="h-100" id="logMap"></log-map>
                     </div>
-                    <div class="col-lg-5 h-100">
+                    <div class="col-lg-5 h-100" id="right-panel">
                         <log-table class="h-100" style="font-size: 12px;font-weight: 400; line-height: 1;"></log-table>
                         <log-details></log-details>
                     </div>
                 </div>
 
         `;
-  }
-
-  async openDb() {
-
+        this.querySelector('log-table').addEventListener('no-flights', (event) => {
+          const rightPanel = this.querySelector('#right-panel');
+          if (rightPanel) {
+            rightPanel.innerHTML = `<div id="no-flights-msg" class="alert alert-info" style="margin-top:2rem;">${event.detail.panelMsg}</div>`;
+          }
+        });        
   }
 
 }
