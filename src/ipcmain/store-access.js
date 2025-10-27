9e5068b6cc3686488517f1e7b6ef6b624c59e3ab
Mac16 <gilouthomas@gmail.com>
2025-10-27 19:36:54 +0100

    invoke store modified

--- src/ipcmain/store-access.js
+++ src/ipcmain/store-access.js
@@ -2,12 +2,12 @@ const { ipcMain } = require('electron');
 const Store = require('electron-store').default;
 const store = new Store();
 
-ipcMain.handle('electron-store-get', (event, key) => {
+ipcMain.handle('store-get', (event, key) => {
     // Pour lire depuis le renderer const dbPath = await window.electronAPI.storeGet('pathdb');
     return store.get(key);
 });
 
-ipcMain.handle('electron-store-set', (event, key, value) => {
+ipcMain.handle('store-set', (event, key, value) => {
     // Pour écrire depuis le renderer await window.electronAPI.storeSet('pathdb', '/Users/gil/Documents/Logfly');
     store.set(key, value);
     return true;
