b18edc66bae23367dd7c4f1855f6dc45aef99399
Mac16 <gilouthomas@gmail.com>
2025-10-28 23:41:06 +0100

    Main div width modified

--- src/components/pages/home-page.js
+++ src/components/pages/home-page.js
@@ -12,10 +12,10 @@ export class HomePage extends HTMLElement {
   connectedCallback() {
         this.innerHTML = /*html */`
                 <div class="row">
-                    <div class="col-lg-7 h-100">
+                    <div class="col-lg-6 h-100">
                          <log-map class="h-100" id="logMap"></log-map>
                     </div>
-                    <div class="col-lg-5 h-100" id="right-panel">
+                    <div class="col-lg-6 h-100" id="right-panel">
                         <log-table class="h-100" style="font-size: 12px;font-weight: 400; line-height: 1;"></log-table>
                         <log-details></log-details>
                     </div>
